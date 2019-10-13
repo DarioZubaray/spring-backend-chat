a3a81e0b4a81baa2736cf7489e8023a4cd3a65cb
dariozubaray <dzubaray@gmail.com>
2019-10-13 17:00:26 -0300

    agregando el metodo handler para saber si un usuario esta escribiendo

--- src/main/java/com/github/dariozubaray/springboot/backend/chat/controllers/ChatController.java
+++ src/main/java/com/github/dariozubaray/springboot/backend/chat/controllers/ChatController.java
@@ -27,4 +27,10 @@ public class ChatController {
         }
         return mensaje;
     }
+    
+    @MessageMapping("/escribiendo")
+    @SendTo("/chat/escribiendo")
+    public String estaEscribiendo(String username) {
+        return username.concat(" está escribiendo...");
+    }
 }
